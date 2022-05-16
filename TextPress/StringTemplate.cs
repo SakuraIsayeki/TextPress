144bb97177cc6d23418f6a300987e130f79e3305
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-16 04:28:48 +0200

    Implement Escaping & Configuration
    So far the strings are escaped as expected, however not substituted for their normal value.

--- TextPress/StringTemplate.cs
+++ TextPress/StringTemplate.cs
@@ -5,28 +5,95 @@ namespace TextPress;
 /// <summary>
 /// Base class for a string template.
 /// </summary>
-public partial class StringTemplate
+public class StringTemplate
 {
-	private const string TemplatePattern = "{(?<variable>[^}]+)}";
-
-#if NET7_0_OR_GREATER
-	[RegexGenerator(TemplatePattern)]
-	private static partial Regex TemplateRegex();
-#else
-	private static Regex TemplateRegex() => _templateRegex;
-	private static readonly Regex _templateRegex = new(TemplatePattern, RegexOptions.Compiled);
-#endif
-	
 	/// <summary>
-	/// Substitutes variables notated as "{variable}" with values from the corresponding dictionary keys.
+	/// Represents the Regular Expression for which variables will be matched.
 	/// </summary>
-	/// <param name="model"></param>
-	/// <param name="values"></param>
-	/// <returns></returns>
-	public static string FillTemplate(string model, IDictionary<string, string> values) 
-		=> TemplateRegex().Replace(model, match =>
+	protected Regex TemplateRegex { get; } = _defaultTemplateRegex;
+
+	private const string DefaultTemplatePattern = "{(?<variable>[^}]+)}";
+	protected static readonly Regex _defaultTemplateRegex = new(DefaultTemplatePattern, RegexOptions.Compiled);
+
+	public static StringTemplate Default { get; } = new(_defaultTemplateRegex);
+
+	/// <summary>
+	/// Instantiates a new StringTemplate using the given options.
+	/// </summary>
+	public StringTemplate(StringTemplateOptions options)
+	{
+		TemplateRegex = BuildTemplateRegex(options);
+	}
+
+	protected internal StringTemplate(Regex regex)
+	{
+		TemplateRegex = regex;
+	}
+
+	/// <summary>
+	/// Builds a Regex from the given options.
+	/// </summary>
+	/// <exception cref="ArgumentOutOfRangeException"></exception>
+	protected internal static Regex BuildTemplateRegex(StringTemplateOptions options)
+	{
+		/*
+		 * Regex is built with the following pattern:
+		 *   - Starts with options.StartDelimiter, ends with options.EndDelimiter
+		 *   - Contains a group named "variable"
+		 *
+		 *   - If EscapingStyle is not None, then the following is added to the pattern:
+		 *     - StartDelimiter indicates that the variable is escaped if the EscapeCharacter is present in front of the variable
+		 *	   - EndDelimiter indicates that the variable is escaped if the EscapeCharacter is present after the variable
+		 *	   - DoubleDelimiters indicates that the variable is escaped if StartDelimiter and EndDelimiter are present twice in a row.
+		 */
+
+		// Escape all options characters
+		string startDelimiter = Regex.Escape(options.StartDelimiter.ToString());
+		string endDelimiter = Regex.Escape(options.EndDelimiter.ToString());
+		string? escapeCharacter = null;
+		
+		if (options is { EscapingStyle: not (VariableEscapingStyle.None or VariableEscapingStyle.DoubleDelimiters), EscapeCharacter: not null })
+		{
+			escapeCharacter = Regex.Escape(options.EscapeCharacter.ToString()!);
+		}
+
+		string pattern = options.EscapingStyle switch
+		{
+			// No escaping
+			VariableEscapingStyle.None => $"{startDelimiter}(?<variable>[^{endDelimiter}]+){endDelimiter}",
+
+			not (VariableEscapingStyle.None or VariableEscapingStyle.DoubleDelimiters) when options.EscapeCharacter is null 
+				=> throw new ArgumentOutOfRangeException(nameof(options), "EscapeCharacter must be specified when EscapingStyle is not None."),
+			
+			// Exclude any string with double delimiters (e.g. {{variable}} will be ignored)
+			VariableEscapingStyle.DoubleDelimiters => $"{startDelimiter}(?<variable>[^{endDelimiter}]+(?<!{startDelimiter}{endDelimiter})){endDelimiter}",
+
+			// Escape if the EscapeCharacter is present in front of the variable (e.g. !{variable} will be ignored)
+			// Use negative lookbehind to ensure that the EscapeCharacter is not the first character in the string
+			VariableEscapingStyle.StartingCharacter => $"(?<!{escapeCharacter}){startDelimiter}(?<variable>[^{endDelimiter}]+){endDelimiter}",
+			
+
+			// Escape if the EscapeCharacter is present after the variable (e.g. {variable}! will be ignored)
+			// Use negative lookahead to ensure that the EscapeCharacter is not the last character in the string
+			VariableEscapingStyle.EndingCharacter => $"{startDelimiter}(?<variable>[^{endDelimiter}]+){endDelimiter}(?!{escapeCharacter})",
+				
+			_ => throw new ArgumentOutOfRangeException(nameof(options.EscapingStyle), options.EscapingStyle, "Invalid escaping style.")
+		};
+
+		return new(pattern, options.RegexOptions);
+	}
+
+	/*
+	 * FIXME: Method is allocating too much memory (552 bytes per call).
+	 */
+	/// <summary>
+	/// Substitutes variables notated as "{variable}" in the model string, with values from the corresponding dictionary keys.
+	/// </summary>
+	public string Fill(string model, IReadOnlyDictionary<string, string> values) => TemplateRegex.Replace(model, match =>
 		{
 			string variable = match.Groups["variable"].Value;
+
 			return values.ContainsKey(variable) ? values[variable] : match.Value;
-		});
+		}
+	);
 }
---
+++ TextPress/StringTemplateOptions.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TextPress;
+
+/// <summary>
+/// Provides configuration for a <see cref="StringTemplate"/> instance.
+/// </summary>
+public record StringTemplateOptions
+{
+	public char StartDelimiter { get; init; } = '{';
+	public char EndDelimiter { get; init; } = '}';
+	public char? EscapeCharacter { get; init; }
+	
+	public VariableEscapingStyle EscapingStyle { get; init; } = VariableEscapingStyle.None;
+	public RegexOptions RegexOptions { get; init; }
+}
+
+/// <summary>
+/// Defines escaping strategies used to properly parse Variables in a <see cref="StringTemplate"/> instance.
+/// </summary>
+public enum VariableEscapingStyle
+{
+	/// <summary>
+	/// No escaping is performed.
+	/// </summary>
+	None,
+	
+	/// <summary>
+	/// Escaping is performed by doubling variable delimiters.
+	/// (e.g: <c>{{variableName}}</c>)
+	/// </summary>
+	DoubleDelimiters,
+	
+	/// <summary>
+	/// Escaping is performed by appending an escape character to the start of a variable.
+	/// (e.g: <c>!{variableName}</c>)
+	/// </summary>
+	StartingCharacter,
+	
+	/// <summary>
+	/// Escaping is performed by appending an escape character to the end of a variable.
+	/// (e.g: <c>{variableName}!</c>)
+	/// </summary>
+	EndingCharacter
+}

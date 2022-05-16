4eb7f5a683ab08d1df886d43f809af39374f651a
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-16 04:29:35 +0200

    Implement Testing for escapes

--- TextPress.Tests/StringTemplateTests.cs
+++ TextPress.Tests/StringTemplateTests.cs
@@ -11,7 +11,7 @@ public class StringTemplateTests
 		const string template = "Hello, {name}!";
 		const string expected = "Hello, World!";
 
-		string result = StringTemplate.FillTemplate(template, new Dictionary<string, string> { { "name", "World" } });
+		string result = StringTemplate.Default.Fill(template, new Dictionary<string, string> { { "name", "World" } });
 		Assert.Equal(expected, result);
 	}
 	
@@ -21,7 +21,47 @@ public class StringTemplateTests
 		const string template = "Hello, {name}! You are {age} years old.";
 		const string expected = "Hello, World! You are 42 years old.";
 
-		string result = StringTemplate.FillTemplate(template, new Dictionary<string, string> { { "name", "World" }, { "age", "42" } });
+		string result = StringTemplate.Default.Fill(template, new Dictionary<string, string> { { "name", "World" }, { "age", "42" } });
 		Assert.Equal(expected, result);
+	}
+	
+	[Fact]
+	public void TestDoubleEscapedTemplateRegex()
+	{
+		const string template = "Hello, {name}! Hello {{name}}!";
+		const string expected = "Hello, world! Hello {{name}}!";
+
+		StringTemplate templateEngine = new(new() { EscapingStyle = VariableEscapingStyle.DoubleDelimiters });
+		Assert.Equal(expected, templateEngine.Fill(template, new Dictionary<string, string> { { "name", "world" } }));
+	}
+	
+	[Fact]
+	public void TestStartEscapedTemplateRegex()
+	{
+		const string template = "Hello, {name}! Hello ${name}!";
+		const string expected = "Hello, world! Hello ${name}!";
+
+		StringTemplate templateEngine = new(new()
+		{
+			EscapingStyle = VariableEscapingStyle.StartingCharacter, 
+			EscapeCharacter = '$'
+		});
+		
+		Assert.Equal(expected, templateEngine.Fill(template, new Dictionary<string, string> { { "name", "world" } }));
+	}
+	
+	[Fact]
+	public void TestEndEscapedTemplateRegex()
+	{
+		const string template = "Hello, {name}! Hello {name}$!";
+		const string expected = "Hello, world! Hello {name}$!";
+
+		StringTemplate templateEngine = new(new()
+		{
+			EscapingStyle = VariableEscapingStyle.EndingCharacter, 
+			EscapeCharacter = '$'
+		});
+		
+		Assert.Equal(expected, templateEngine.Fill(template, new Dictionary<string, string> { { "name", "world" } }));
 	}
 }

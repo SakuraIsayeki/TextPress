71264a6abe01c487573189f71ded90b2b0f645d1
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-16 09:54:17 +0200

    Added extra benchmarks

--- TextPress.Benchmarks/StringTemplateBenchmarks.cs
+++ TextPress.Benchmarks/StringTemplateBenchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using TextPress;
@@ -12,9 +13,28 @@ namespace TextPress.Benchmarks;
 [MemoryDiagnoser, GcServer(true)]
 public class StringTemplateBenchmarks
 {
-	private static readonly Dictionary<string, string> values = new() { { "name", "World" } };
+	private static readonly Dictionary<string, string> _values = new() { { "name", "World" } };
 
-	[Benchmark]
-	public string FillTemplateCompiledRegex() => StringTemplate.Default.Fill("Hello, {name}!", values);
+	private static readonly StringTemplateFactory _factory = new();
 	
+	[GlobalSetup]
+	public void Setup()
+	{
+		// Pre-initialize all templates.
+		_factory.GetTemplate("");
+		_factory.GetTemplate("");
+		_factory.GetTemplate("custom", new()
+		{
+			StartDelimiter = "${",
+			EndDelimiter = "}*",
+			EscapeCharacter = '!',
+			EscapingStyle = VariableEscapingStyle.StartingCharacter,
+			RegexOptions = RegexOptions.Compiled
+		});
+	}
+	
+	[Benchmark] public string FillSimpleTemplate() => StringTemplate.Default.Fill("Hello, {name}!", _values);
+	[Benchmark] public string FillSimpleFactoryTemplate() => _factory.GetTemplate("").Fill("Hello, {name}!", _values);
+	[Benchmark] public string FillCustomTemplate() => _factory.GetTemplate("custom").Fill("Hello, ${name}*!", _values);
+
 }
